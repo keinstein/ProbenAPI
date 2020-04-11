dce152c2670aa2fef3361ccef635bc5e66cc1f72
Tobias Schlemmer <Tobias.Schlemmer@schlemmersoft.de>
2020-04-11 22:26:08 +0200

    re-add module-info for ProbenAPI

---
+++ ProbenFile.module-info.java
@@ -0,0 +1,6 @@
+module de.schlemmersoft.bewerbung.test1.ProbenFile {
+    requires de.schlemmersoft.bewerbung.test1.ProbenAPI;
+    requires de.schlemmersoft.bewerbung.test1.ProbenVector;
+
+    exports de.schlemmersoft.bewerbung.test1.Proben.File;
+}
