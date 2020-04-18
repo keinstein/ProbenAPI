2a9d2f3588dc36a416c379430526da5a9ff4c276
Tobias Schlemmer <Tobias.Schlemmer@schlemmersoft.de>
2020-04-18 17:48:59 +0200

    First step to implement SQL backend for ProbenAPI

--- ProbenSQL/src/main/java/de/schlemmersoft/bewerbung/test1/Proben/SQL/ProbenSQL.java
+++ ProbenSQL/src/main/java/de/schlemmersoft/bewerbung/test1/Proben/SQL/ProbenSQL.java
@@ -1,9 +1,276 @@
 package de.schlemmersoft.bewerbung.test1.Proben.SQL;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.time.ZonedDateTime;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.UUID;
+
+import de.schlemmersoft.bewerbung.test1.Proben.Public.ProbenAPI;
+import de.schlemmersoft.bewerbung.test1.Proben.Public.ProbenAPI.Probe.Interpretation;
+
 /**
  * Hello world!
  *
  */
-public class ProbenSQL
+public class ProbenSQL implements ProbenAPI<Integer>
 {
+
+	public class SQLProbe implements Probe<Integer> {
+		String id;
+
+		SQLProbe(String newid) {
+			id = newid;
+		}
+
+		public boolean equals(Object other) {
+			if (other instanceof SQLProbe)
+				return id.equals(((SQLProbe)other).id);
+			return false;
+		}
+
+		@Override
+		public boolean equals(Probe<Integer> other) {
+			return getValue().equals(other.getValue());
+		}
+
+		@Override
+		public Probe<Integer> clone() throws CloneNotSupportedException {
+			SQLProbe newprobe = (ProbenSQL.SQLProbe)super.clone();
+			newprobe.id = id;
+			return newprobe;
+		}
+
+		@Override
+		public String getID() {
+			return id;
+		}
+
+		@Override
+		public ZonedDateTime getTime() {
+			try {
+				if (SQLProbeGetTime == null)
+					SQLProbeGetTime = connection.prepareStatement("SELECT time FROM " + tableName + " WHERE id = ?");
+				SQLProbeGetTime.setString(1, id);
+				ResultSet res = SQLProbeGetTime.executeQuery();
+				res.next();
+				return ZonedDateTime.parse(res.getString(1));
+			} catch (SQLException e){
+				// TODO: implement error handling
+				return null;
+			}
+		}
+
+		@Override
+		public Integer getValue() {
+			try {
+				if (SQLProbeGetValue == null)
+					SQLProbeGetValue = connection.prepareStatement("SELECT value FROM " + tableName + " WHERE id = ?");
+				SQLProbeGetValue.setString(1, id);
+				ResultSet res = SQLProbeGetValue.executeQuery();
+				res.next();
+				int i = res.getInt(1);
+				if (res.wasNull()) return null;
+				return Integer.valueOf(i);
+			} catch (SQLException e){
+				// TODO: implement error handling
+				return null;
+			}
+
+		}
+
+		@Override
+		public void setValue(Integer v) {
+			try {
+				if (SQLProbeSetValue == null)
+					SQLProbeSetValue = connection.prepareStatement("UPDATE " + tableName + " SET value = ? WHERE id = ?");
+				SQLProbeSetValue.setInt(0, v.intValue());
+				SQLProbeSetValue.setString(1, id);
+				SQLProbeGetValue.executeUpdate();
+			} catch (SQLException e){
+				// TODO: implement error handling
+			}
+		}
+
+		@Override
+		public Interpretation getInterpretation() {
+			Integer v = getValue();
+			if (v == null) return Interpretation.FUZZY;
+			int value = v.intValue();
+			if (value < 0) return Interpretation.BAD;
+			if (value > 0 ) return Interpretation.GOOD;
+			return Interpretation.FUZZY;
+		}
+
+		@Override
+		public String toString() {
+			return "Probe " + id;
+		}
+	}
+
+	class ProbenIterator implements Iterator<Probe<Integer>> {
+		ResultSet queryResult;
+		String current;
+
+		ProbenIterator (ResultSet res) {
+			current = null;
+			queryResult = res;
+			try {
+				queryResult.next();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				throw new NoSuchElementException();
+			}
+		}
+
+		public boolean hasNext() {
+			try {
+				return !queryResult.isAfterLast();
+			} catch (SQLException e) {
+				e.printStackTrace();
+				return false;
+			}
+		}
+
+		@Override
+		public SQLProbe next() {
+			if (!hasNext())
+				throw new NoSuchElementException();
+			try {
+				current = queryResult.getString("id");
+				queryResult.next();
+				return new SQLProbe(current);
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				throw new NoSuchElementException();
+			}
+		}
+	}
+
+	private Connection connection;
+	private Statement statement;
+	private PreparedStatement SQLProbeGetId;
+	private PreparedStatement SQLProbeGetTime;
+	private PreparedStatement SQLProbeGetValue;
+	private PreparedStatement SQLProbeSetValue;
+	private PreparedStatement SQLProbeGetAllIds;
+	private PreparedStatement SQLProbeNew;
+	String tableName;
+
+	ProbenSQL (Connection c, String tablename) {
+		connection = c;
+		tableName = tablename;
+	}
+
+	ProbenSQL (String uri, String tablename) throws SQLException {
+		 // create a database connection
+		connection = DriverManager.getConnection("jdbc:sqlite:sample.db");
+		try {
+			statement = connection.createStatement();
+			statement.setQueryTimeout(30);  // set timeout to 30 sec.
+		} catch (SQLException e) {
+			connection = null;
+			throw e;
+		}
+		tableName = tablename;
+	}
+
+	void clearTable() throws SQLException {
+		statement.executeUpdate("DROP TABLE IF EXISTS "+ tableName);
+        statement.executeUpdate("CREATE TABLE " + tableName + "(id STRING, time STRING, value INTEGER)");
+	}
+
+	@Override
+	public Iterator<Probe<Integer>> iterator() {
+		try {
+			if (SQLProbeGetAllIds == null)
+				SQLProbeGetAllIds = connection.prepareStatement("SELECT id FROM " + tableName + " ORDER BY time");
+			ResultSet res = SQLProbeGetAllIds.executeQuery();
+			// TODO Auto-generated method stub
+			return new ProbenIterator(res);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	@Override
+	public Iterable<Probe<Integer>> range(Integer min, Integer max) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Iterable<Probe<Integer>> result(Interpretation key) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Probe<Integer> add(Probe<Integer> sample) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public SQLProbe add(ZonedDateTime time) {
+		try {
+			if (SQLProbeGetId == null)
+					SQLProbeGetId = connection.prepareStatement("SELECT count(*) FROM " + tableName + " WHERE id = ? ORDER BY time");
+			String id;
+			ResultSet res;
+			do {
+				UUID uuid = UUID.randomUUID();
+				id = uuid.toString();
+				SQLProbeGetId.setString(1,id);
+				res = SQLProbeGetId.executeQuery();
+				res.next();
+			} while (res.getInt(1) > 0);
+			return add(id,time);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	@Override
+	public SQLProbe add(String id, ZonedDateTime time) {
+		try {
+			if (SQLProbeNew == null)
+				SQLProbeNew = connection.prepareStatement("INSERT into " + tableName + " (id,time) VALUES (?,?)");
+			SQLProbeNew.setString(1,id);
+			SQLProbeNew.setString(2,time.toString());
+			if (SQLProbeNew.executeUpdate() > 0)
+				return new SQLProbe(id);
+			else
+				return null;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	@Override
+	public void remove(String id) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void remove(Probe<Integer> sample) {
+		// TODO Auto-generated method stub
+
+	}
+
+
 }
---
+++ ProbenSQL/src/test/java/de/schlemmersoft/bewerbung/test1/Proben/SQL/TestProbenSQL.java
@@ -0,0 +1,434 @@
+package de.schlemmersoft.bewerbung.test1.Proben.SQL;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.time.ZonedDateTime;
+import java.util.Iterator;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import de.schlemmersoft.bewerbung.test1.Proben.Public.ProbenAPI.Probe;
+import de.schlemmersoft.bewerbung.test1.Proben.SQL.ProbenSQL.SQLProbe;
+
+/**
+ * Unit test for simple App.
+ */
+public class TestProbenSQL
+{
+	ProbenSQL data;
+
+	@Test
+	void TestInstantiateWithNew() throws SQLException {
+		data = new ProbenSQL("jdbc:sqlite:sample.db","testProbenSQL");
+	}
+
+	@Nested
+	public class WhenNew {
+		@BeforeEach
+		void createNewSQL() throws SQLException {
+			data = new ProbenSQL("jdbc:sqlite:sample.db","testProbenSQL");
+			data.clearTable();
+		}
+
+		SQLProbe getOnlyEntry () {
+			Iterator<Probe<Integer>> it =  data.iterator();
+			assertTrue(it.hasNext());
+			SQLProbe retval = (SQLProbe) it.next();
+			assertFalse(it.hasNext());
+			return retval;
+		}
+
+/*
+		@Test
+		void TestIteratorEmpty() {
+			Iterator<Probe<Integer>> it =  data.iterator();
+			assertFalse(it.hasNext());
+			assertThrows(NoSuchElementException.class,
+					() -> { it.next(); });
+		}
+
+		@Test
+		void TestRangeIteratorEmpty() {
+			Iterator<Probe<Integer>> it =
+					data.range(new Integer(-5), new Integer(9)).iterator();
+			assertFalse(it.hasNext());
+			assertThrows(NoSuchElementException.class,
+					() -> { it.next(); });
+		}
+
+		@ParameterizedTest(name="Run {index}: testId={0}")
+		@EnumSource(Interpretation.class)
+		void TestResultIteratorEmpty(Interpretation i) {
+		    Iterator<Probe<Integer>> it = data.result(i).iterator();
+			assertFalse(it.hasNext());
+			assertThrows(NoSuchElementException.class,
+					() -> { it.next(); });
+		}
+*/
+		@Test
+		void addByDate() {
+			assertNotNull(data);
+			ZonedDateTime time = ZonedDateTime.now();
+			assertNotNull(time);
+			SQLProbe origsample = data.add(time);
+			SQLProbe sample = getOnlyEntry();
+			assertEquals(origsample,sample);
+			assertNotEquals("", sample.getID());
+			assertEquals(time,sample.getTime());
+			assertNull(sample.getValue());
+
+			// check order
+			data.add(time.minusHours(1));
+			data.add(time.plusHours(1));
+			data.add(time);
+
+			Iterator<Probe<Integer>> it = data.iterator();
+			assertTrue(it.hasNext());
+			assertEquals(time.minusHours(1),((SQLProbe)it.next()).getTime());
+			assertTrue(it.hasNext());
+			assertEquals(time,((SQLProbe)it.next()).getTime());
+			assertTrue(it.hasNext());
+			assertEquals(time,((SQLProbe)it.next()).getTime());
+			assertTrue(it.hasNext());
+			assertEquals(time.plusHours(1),((SQLProbe)it.next()).getTime());
+			assertFalse(it.hasNext());
+		}
+/*
+		@Test
+		void addByIdDate() {
+			ZonedDateTime time = ZonedDateTime.now();
+			SQLProbe origsample = data.add("Sample 1",time);
+			SQLProbe sample = getOnlyEntry();
+			assertTrue(sample == origsample);
+			assertEquals("Sample 1",sample.getID());
+			assertEquals(time,sample.getTime());
+			assertNull(sample.getValue());
+
+			assertThrows(IllegalArgumentException.class,
+					() -> { data.add("Sample 1",time); });
+		}
+
+		@Test
+		void addByDateValue() {
+			ZonedDateTime time = ZonedDateTime.now();
+			SQLProbe origsample = data.add(time,new Integer(3));
+			SQLProbe sample = getOnlyEntry();
+			assertTrue(sample == origsample);
+			assertNotEquals("", sample.getID());
+			assertEquals(time,sample.getTime());
+			assertEquals(new Integer(3),sample.getValue());
+
+			// 2nd element should be after 1st.
+			data.add(time.plusHours(1),new Integer(5));
+			data.add(time,new Integer(4));
+			Iterator<Probe<Integer>> it = data.iterator();
+			assertTrue(it.hasNext());
+			Integer expectedValue = new Integer(3);
+			assertEquals(expectedValue,((SQLProbe)it.next()).getValue());
+			assertTrue(it.hasNext());
+			expectedValue = new Integer(4);
+			assertEquals(expectedValue,((SQLProbe)it.next()).getValue());
+			assertTrue(it.hasNext());
+			expectedValue = new Integer(5);
+			assertEquals(new Integer(5),((SQLProbe)it.next()).getValue());
+			assertFalse(it.hasNext());
+		}
+
+		@Test
+		void addByAll() {
+			ZonedDateTime time = ZonedDateTime.now();
+			assertNotNull(time);
+			SQLProbe origsample = data.add("Sample 2",time,new Integer(-18));
+			assertNotNull(origsample);
+			SQLProbe sample = getOnlyEntry();
+			assertNotNull(sample);
+			assertTrue(sample == origsample);
+			assertEquals("Sample 2",sample.getID());
+			assertEquals(time,sample.getTime());
+			assertEquals(new Integer(-18),sample.getValue());
+
+			assertThrows(IllegalArgumentException.class,
+					() -> { data.add("Sample 2",time,new Integer(-18)); });
+		}
+
+		@Test
+		void addByObject() {
+			ZonedDateTime time = ZonedDateTime.now();
+			SQLProbe original = new SQLProbe("Sample 3",time,new Integer(79));
+			SQLProbe inserted = data.add(original);
+			assertFalse(original == inserted);
+			SQLProbe sample = getOnlyEntry();
+			assertTrue(sample == inserted);
+			assertEquals("Sample 3",sample.getID());
+			assertEquals(time,sample.getTime());
+			assertEquals(new Integer(79),sample.getValue());
+
+			assertThrows(IllegalArgumentException.class,
+					() -> { data.add(original); });
+		}
+
+		@Nested
+		class WhenAdd {
+			class TestSQLProbe {
+				public String id;
+				public ZonedDateTime time;
+				public Integer value;
+
+				TestSQLProbe(String s, ZonedDateTime t, Integer v) {
+					id = s;
+					time = t;
+					value = v;
+				}
+				TestSQLProbe(String s, ZonedDateTime t) {
+					id = s;
+					time = t;
+					value = null;
+				}
+			}
+			ArrayList<TestSQLProbe> samples;
+
+			@BeforeEach
+			void fillDatabase() {
+				ZonedDateTime time = ZonedDateTime.now();
+				Stream<TestSQLProbe> sampleStream =
+						Stream.of(
+						 new TestSQLProbe((String)null, time.plusHours(1)),
+						 new TestSQLProbe((String)null, time.minusHours(2)),
+						 new TestSQLProbe((String)null, time.plusHours(2)),
+						 new TestSQLProbe((String)null, time.plusHours(7)),
+						 new TestSQLProbe("Sample 1",time.plusHours(4)),
+						 new TestSQLProbe((String)null, time,new Integer(3)),
+						 new TestSQLProbe((String)null, time.plusHours(3),new Integer(5)),
+						 new TestSQLProbe((String)null, time.plusHours(6),new Integer(4)),
+						 new TestSQLProbe("Sample 2",time.minusHours(1),new Integer(-18)),
+						 new TestSQLProbe("Sample 3",time.plusHours(5),new Integer(79))
+			        );
+				samples = sampleStream.collect(Collectors.toCollection(ArrayList::new));
+				for (TestSQLProbe sample : samples) {
+					if (sample.id == null) {
+						if (sample.value == null)
+							data.add(sample.time);
+						else
+							data.add(sample.time,sample.value);
+					} else {
+						if (sample.id.equals("Sample 3"))
+							data.add(new SQLProbe(sample.id,sample.time,sample.value));
+						else if (sample.value == null)
+							data.add(sample.id,sample.time);
+						else
+							data.add(sample.id,sample.time,sample.value);
+					}
+				}
+
+				Collections.sort(samples, (Comparator<? super TestSQLProbe>) (TestSQLProbe a, TestSQLProbe b) -> a.time.compareTo(b.time));
+			}
+
+			@Test
+			void orderedIterator() {
+				Iterator<Probe<Integer>> it = data.iterator();
+				Iterator<TestSQLProbe> it2 = samples.iterator();
+
+				// don't use hasNext on it (this execution path must be checked, too)
+				while(it2.hasNext()) {
+					Probe<Integer> sample1 = it.next();
+					TestSQLProbe sample2 = it2.next();
+					if (sample2.id != null) {
+						assertEquals(sample1.getID(),sample2.id);
+					}
+					assertEquals(sample1.getTime(),sample2.time);
+					assertEquals(sample1.getValue(),sample2.value);
+				}
+				assertThrows(NoSuchElementException.class,
+						() -> { it.next(); });
+			}
+
+			@Test
+			void testRangeIterator() {
+				Iterator<Probe<Integer>> it = data.range(new Integer(3),new Integer(5)).iterator();
+				Iterator<TestSQLProbe> it2 = samples.iterator();
+
+				// don't use hasNext on it (this execution path must be checked, too)
+				while(it2.hasNext()) {
+					TestSQLProbe sample2 = it2.next();
+					if (sample2.value == null) continue;
+					if (sample2.value.value < 3 || sample2.value.value > 5)
+						continue;
+					Probe<Integer> sample1 = it.next();
+					assertEquals(sample2.value,sample1.getValue());
+					assertEquals(sample2.time,sample1.getTime());
+					if (sample2.id != null) {
+						assertEquals(sample2.id,sample1.getID());
+					}
+				}
+				assertThrows(NoSuchElementException.class,
+						() -> { it.next(); });
+			}
+
+			@ParameterizedTest(name="Run {index}: testId={0}")
+			@EnumSource(Interpretation.class)
+			void testInterpretationIterator(Interpretation interpretation) {
+				Iterator<Probe<Integer>> it = data.result(interpretation).iterator();
+				Iterator<TestSQLProbe> it2 = samples.iterator();
+
+				// don't use hasNext on it (this execution path must be checked, too)
+				while(it2.hasNext()) {
+					TestSQLProbe sample2 = it2.next();
+					if (interpretation != Interpretation.FUZZY && sample2.value == null)
+						continue;
+					if (sample2.value != null)
+						switch (interpretation) {
+						case GOOD:
+							if (sample2.value.value <= 0) continue;
+							break;
+						case BAD:
+							if (sample2.value.value >= 0) continue;
+							break;
+						case FUZZY:
+							if (sample2.value.value != 0) continue;
+							break;
+						}
+					Probe<Integer> sample1 = it.next();
+					assertEquals(sample2.value,sample1.getValue());
+					assertEquals(sample2.time,sample1.getTime());
+					if (sample2.id != null) {
+						assertEquals(sample2.id,sample1.getID());
+					}
+				}
+				assertThrows(NoSuchElementException.class,
+						() -> { it.next(); });
+			}
+
+
+			@Test
+			void testRemoveNonexistingId() {
+				assertThrows(NoSuchElementException.class,
+						() -> { data.remove(""); });
+				assertThrows(NoSuchElementException.class,
+						() -> { data.remove(new SQLProbe("",ZonedDateTime.now())); });
+			}
+
+			@RepeatedTest(10)
+			void testDeleteById (RepetitionInfo repetitionInfo ) {
+				System.out.print("Current rep. ");
+				System.out.println(repetitionInfo.getCurrentRepetition());
+				TestSQLProbe current = samples.get(repetitionInfo.getCurrentRepetition()-1);
+				if (current.id == null) return;
+				assertTrue(data.size() >= 10);
+				int oldsize = data.size();
+				data.remove(current.id);
+				assertEquals(oldsize-1, data.size());
+
+				Iterator<Probe<Integer>> it = data.iterator();
+				Probe<Integer> sample1;
+
+				for (int i = 0 ; i != samples.size(); ++i) {
+					System.out.printf("#%d/%d: \n",i,repetitionInfo.getCurrentRepetition());
+					if (i == repetitionInfo.getCurrentRepetition()-1) continue;
+					current = samples.get(i);
+					sample1 = it.next();
+					System.out.print(current.id);
+					System.out.print(" == ");
+					System.out.println(sample1);
+					assertEquals(current.value,sample1.getValue());
+					assertEquals(current.time,sample1.getTime());
+					if (current.id != null) {
+						assertEquals(current.id,sample1.getID());
+					}
+				}
+				assertThrows(NoSuchElementException.class,
+						() -> { it.next(); });
+			}
+
+			@RepeatedTest(10)
+			void testDeleteByObject (RepetitionInfo repetitionInfo ) {
+				System.out.print("Current rep. ");
+				System.out.println(repetitionInfo.getCurrentRepetition());
+
+				assertTrue(data.size() >= 10);
+				Probe<Integer> sample = null;
+				Iterator<Probe<Integer>> it = data.iterator();
+				for (int i = repetitionInfo.getCurrentRepetition(); i > 0; --i) {
+					sample = it.next();
+				}
+				assertNotNull(sample);
+
+				int oldsize = data.size();
+				data.remove(sample);
+				assertEquals(oldsize-1, data.size());
+
+				it = data.iterator();
+				TestSQLProbe current;
+				for (int i = 0 ; i != samples.size(); ++i) {
+					System.out.printf("#%d/%d: \n",i,repetitionInfo.getCurrentRepetition());
+					if (i == repetitionInfo.getCurrentRepetition()-1) continue;
+					current = samples.get(i);
+					sample = it.next();
+					System.out.print(current.id);
+					System.out.print(" == ");
+					System.out.println(sample);
+					assertEquals(current.value,sample.getValue());
+					assertEquals(current.time,sample.getTime());
+					if (current.id != null) {
+						assertEquals(current.id,sample.getID());
+					}
+				}
+				Iterator<Probe<Integer>> it2 = it;
+				assertThrows(NoSuchElementException.class,
+						() -> { it2.next(); });
+			}
+		}
+*/
+
+	}
+
+	@Test
+    public static void sqliteExample()
+    {
+      Connection connection = null;
+      try
+      {
+        // create a database connection
+        connection = DriverManager.getConnection("jdbc:sqlite:sample.db");
+        Statement statement = connection.createStatement();
+        statement.setQueryTimeout(30);  // set timeout to 30 sec.
+
+        statement.executeUpdate("drop table if exists person");
+        statement.executeUpdate("create table person (id integer, name string)");
+        statement.executeUpdate("insert into person values(1, 'leo')");
+        statement.executeUpdate("insert into person values(2, 'yui')");
+        ResultSet rs = statement.executeQuery("select * from person");
+        while(rs.next())
+        {
+          // read the result set
+          System.out.println("name = " + rs.getString("name"));
+          System.out.println("id = " + rs.getInt("id"));
+        }
+      }
+      catch(SQLException e)
+      {
+        // if the error message is "out of memory",
+        // it probably means no database file is found
+        System.err.println(e.getMessage());
+      }
+      finally
+      {
+        try
+        {
+          if(connection != null)
+            connection.close();
+        }
+        catch(SQLException e)
+        {
+          // connection close failed.
+          System.err.println(e.getMessage());
+        }
+      }
+    }
+}
