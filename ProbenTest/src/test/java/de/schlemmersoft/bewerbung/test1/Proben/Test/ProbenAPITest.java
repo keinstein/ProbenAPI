a249c72dfa1aa9ea123c63b95a2d818ee9d75b76
Tobias Schlemmer <Tobias.Schlemmer@schlemmersoft.de>
2020-04-22 15:21:33 +0200

    Fix test app test cases for general usage.

--- ProbenTest/src/test/java/de/schlemmersoft/bewerbung/test1/Proben/Test/ProbenAPITest.java
+++ ProbenTest/src/test/java/de/schlemmersoft/bewerbung/test1/Proben/Test/ProbenAPITest.java
@@ -6,6 +6,8 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.sql.SQLException;
 import java.time.ZonedDateTime;
 import java.util.stream.Stream;
@@ -28,85 +30,32 @@ import de.schlemmersoft.bewerbung.test1.Proben.Vector.ProbenVector;
  */
 public class ProbenAPITest
 {
+
 	@Test
 	void testMain() throws FileNotFoundException, SQLException, SyntaxError {
+		String resourceName = "commands1.txt";
+
+		ClassLoader classLoader = getClass().getClassLoader();
+		File file = new File(classLoader.getResource(resourceName).getFile());
 		String[] args = null;
 	    final InputStream original = System.in;
-	    final FileInputStream fips = new FileInputStream(new File("src/test/text/commands1.txt"));
+	    final FileInputStream fips = new FileInputStream(file);
 	    System.setIn(fips);
 	    ConsoleApp.mainProgram(args);
-	    String[] args2 = { ":memory:", "testTable" };
-	    final FileInputStream fips2 = new FileInputStream(new File("src/test/text/commands1.txt"));
-	    System.setIn(fips2);
-	    ConsoleApp.mainProgram(args2);
 	    System.setIn(original);
 	}
-	/*
-		class Probe extends GenericProbe<Integer>{
 
-		}
-	class Mwt implements Messwert, Cloneable {
-		int value;
+	@Test
+	void testMain2() throws FileNotFoundException, SQLException, SyntaxError {
+		String resourceName = "commands1.txt";
 
-		Mwt(int v) {
-			value = v;
-		}
-
-		@Override
-		public boolean equals (Object other) {
-			if (this == other) return true;
-			if (other instanceof Mwt) return equals((Mwt)other);
-			return false;
-		}
-
-		@Override
-		public boolean equals(Messwert other) {
-			if (other instanceof Mwt) return equals((Mwt)other);
-			return false;
-		}
-
-		public boolean equals(Mwt other) {
-			return value == other.value;
-		}
-
-
-		@Override
-		public Mwt clone() {
-			return new Mwt(value);
-		}
-
-		@Override
-		public Interpretation getInterpretation() {
-			// TODO Auto-generated method stub
-			if (value < 0) return Interpretation.BAD;
-			if (value > 0) return Interpretation.GOOD;
-			return Interpretation.FUZZY;
-		}
+		ClassLoader classLoader = getClass().getClassLoader();
+		File file = new File(classLoader.getResource(resourceName).getFile());
+	    String[] args = { ":memory:", "testTable" };
+		final InputStream original = System.in;
+		final FileInputStream fips = new FileInputStream(file);
+		System.setIn(fips);
+	    ConsoleApp.mainProgram(args);
+	    System.setIn(original);
 	}
-
-	static Stream<Arguments> testFullApi_Parameters() throws Throwable {
-        return Stream.of(
-			 Arguments.of(new ProbenVector<Mwt>())/*,
-			 Arguments.of("1", ZonedDateTime.now(),  0, Interpretation.FUZZY),
-			 Arguments.of("2", ZonedDateTime.now(), -1, Interpretation.BAD)*
-			 );
-	}
-
-    /*
-     * Rigorous Test :-)
-     *
-	@ParameterizedTest(name="Run {index}: testId={0}")
-	@MethodSource("testFullApi_Parameters")
-    public void testFullApi(ProbenAPI api)
-    {
-		ZonedDateTime time = ZonedDateTime.now();
-		Probe sample1 = api.add(time);
-		Probe sample2 = api.add("Sample id 1",time.plusHours(1));
-		Probe sample3 = api.add("Sample id 2",time.plusHours(2));
-		sample3.setValue(api.makeValue(3));
-		Probe sample4 = api.add(time.plusHours(3),-3);
-		Probe sample5 = api.add(time.plusHours(4),0);
-		Probe sample6 = api.add(sample4);
-    }
-    */
 }
